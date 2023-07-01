cf79427507a4dd16268e2b0674879926e4389d9a
EstebanMa12 <desteban.maya@udea.edu.co>
2023-06-30 21:32:12 -0500

    Se termina el formulario de citas

--- ejercicioFORMULARIO/script.js
+++ ejercicioFORMULARIO/script.js
@@ -20,3 +20,37 @@ const capturarDatos =() =>{
 // Hacer el evento submit al formulario para que me capture los datos 
 
 // Hacer que se me muestre la información almacenada en el localstorage por medio de un innerHTML y se me refleje en la sesión de agendar 
+
+formulario.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    capturarDatos();
+});
+
+function getLocalStorage() {
+    listarCita.innerHTML= '';
+    citas.forEach((cita,index)=>{
+        const{nombre, fecha,hora,sintoma}=cita;
+        listarCita.innerHTML +=`
+        <tr>
+            <td>${nombre}</td>
+            <td>${fecha}</td>
+            <td>${hora}</td>
+            <td>${sintoma}</td>
+            <td>
+                <button onClick = "eliminarCita(${index}) class= "btn btn-danger">Elimina</button>
+            </td>
+        </tr>
+        `;
+    });
+};
+
+document.addEventListener('DOMContentLoaded',()=>{
+    citas = JSON.parse(localStorage.getItem('citas'))||[];
+    getLocalStorage();
+})
+
+function eliminarCita(index) {
+    citas.splice(index,1);
+    localStorage.setItem("citas",JSON.stringify(citas));
+    getLocalStorage();
+}
