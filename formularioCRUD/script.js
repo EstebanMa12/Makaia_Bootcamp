2e5595c75cc364838d6a3b61d04d49de899f0bfd
EstebanMa12 <desteban.maya@udea.edu.co>
2023-07-17 21:22:52 -0500

    Petición POST

--- formularioCRUD/script.js
+++ formularioCRUD/script.js
@@ -1,40 +1,41 @@
 let formulario = document.getElementById('formulario')
-let btnemail= document.getElementById('btnCorreo')
-let btEditar= document.getElementById('btnEditar')
-let btEliminar= document.getElementById('btnEliminar')
+let btnCorreo = document.getElementById('btnCorreo')
+let btnEditar = document.getElementById('btnEditar')
+let btnEliminar = document.getElementById('btnEliminar')
 
-
-document.addEventListener('DOMContentLoaded',()=>{
-    document.getElementById('id').style.display='none';
-    document.getElementById('label-edit').style.display='none';
-    document.getElementById('id').readOnly=true;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('id').style.display = 'none'
+    document.getElementById('label-edit').style.display = 'none'
+    document.getElementById('id').readOnly = true
 })
+
 let email = document.getElementById('email')
 
-email.addEventListener('input',()=>{
-    document.getElementById('id').style.display='none'
-    document.getElementById('label-edit').style.display='none'
+email.addEventListener('input', () => {
+    document.getElementById('id').style.display = 'none'
+    document.getElementById('label-edit').style.display = 'none'
 })
 
-
-formulario.addEventListener('submit', async e=>{
+formulario.addEventListener('submit', async e => {
     e.preventDefault()
     let name = document.getElementById('name').value
-    let lastname=document.getElementById('lastName').value
-    let email=document.getElementById('email').value
+    let lastName = document.getElementById('lastName').value
+    let email = document.getElementById('email').value
 
-    let  resp = await fetch('http://localhost:3000/usuarios/',{
+    let resp = await fetch('http://localhost:3000/usuarios/', {
         method: 'POST',
         body: JSON.stringify({
-            nombre:name,
-            apellido:lastname,
-            correo:email
+            nombre: name,
+            apellido: lastName,
+            correo: email
         }),
-        headers:{
+        headers: {
             "Content-Type": "application/json; charset=UTF-8"
         }
     })
+
     console.log(resp);
+
     let data = resp.json()
     console.log(data);
 })
